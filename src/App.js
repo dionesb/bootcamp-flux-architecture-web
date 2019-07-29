4bd895f73639ca0b7a3693f456c3ac6f76ab2e38
dionesb <diones.breda@gmail.com>
2019-07-29 18:00:10 -0300

    Saga navigation added.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 /* Importação do Provider do react-redux para deixa-lo disponível para toda a aplicação. */
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
@@ -10,17 +10,18 @@ import GlobalStyle from './styles/globals';
 import Header from './components/Header';
 import Routes from './routes';
 
+import history from './services/history';
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Routes />
         <GlobalStyle />
         <ToastContainer autoClose={3000} />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }
