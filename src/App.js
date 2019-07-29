9b69daded80ea5dcc742655f715eb1a2b5fc5879
dionesb <diones.breda@gmail.com>
2019-07-29 11:48:25 -0300

    Reactotron + Redux added.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter } from 'react-router-dom';
 /* Importação do Provider do react-redux para deixa-lo disponível para toda a aplicação. */
 import { Provider } from 'react-redux';
 
+import './config/reactotronConfig';
+
 import GlobalStyle from './styles/globals';
 import Header from './components/Header';
 import Routes from './routes';
